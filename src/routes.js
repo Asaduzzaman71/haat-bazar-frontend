3ddbd93a0ab38c1a68bc5fac23057ccc90ada545
AsaduzzamanRabbi <asad@merchantbay.com>
2022-04-10 21:56:15 +0600

    logout api integration

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,23 @@
-import DashBoard from './pages/DashbBoard.vue';
 import LoginPage from  './pages/Login.vue';
 import SignUp from './pages/SignUp.vue';
+import DashBoard from './pages/DashbBoard.vue';
 import CategoryIndex from './pages/Category.vue';
 import ProductPage from './pages/Product.vue';
 
-function guardMyroute(to, from, next){
+function guardMyroute(to, from, next)
+{
     var isAuthenticated= false;
-    const token = localStorage.getItem('access-token');
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    var expiaryTime = JSON.parse(jsonPayload);
-    if( localStorage.getItem('access-token') && ( expiaryTime.exp < Date.now() ) ){
+    if(localStorage.getItem('access-token')){
         isAuthenticated = true;
+    }else{
+        isAuthenticated= false;
     }
-    isAuthenticated ?  next() : next('/login');
+    if(isAuthenticated){
+        next(); // allow to enter route
+    }
+    else{
+        next('/login'); // go to '/login';
+    }
 }
 
 export const routes = [
